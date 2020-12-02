c39a6b756ad7d6b23fe93238ef92760d08fb14b9
galinabee <galinab@vt.edu>
2020-12-01 21:41:03 -0800

    cleanup

--- dayOne/sum2020input.py
+++ dayOne/sum2020input.py
@@ -9,7 +9,7 @@ def populate_from_file():
 def find_two_values(input_set):
     for val in input_set:
         if 2020-val in input_set:
-            return val
+            return [val, 2020-val]
 
 def find_three_values(input_set):
     for val1 in input_set:
@@ -17,16 +17,22 @@ def find_three_values(input_set):
             if 2020-val1-val2 in input_set:
                 return [val1, val2, 2020-val1-val2]
 
+def multiply_list_values(input_list):
+    product = 1
+    for val in input_list:
+        product *= val
+    return product
+
 def main():
     print(os.getcwd())
     input_set = populate_from_file()
-    val = find_two_values(input_set)
-    print("The two values are {0} and {1}".format(val, 2020-val))
-    print("Multiplying the two is {0}".format((2020-val)*val))
+    value_list = find_two_values(input_set)
+    print("The two values are {0} and {1}".format(value_list[0], value_list[1]))
+    print("Multiplying the two is {0}".format(multiply_list_values(value_list)))
 
     value_list = find_three_values(input_set)
     print("The three values are {0}, {1} and {2}".format(value_list[0], value_list[1], value_list[2]))
-    print("Multiplying the three is {0}".format(value_list[0]*value_list[1]*value_list[2]))
+    print("Multiplying the three is {0}".format(multiply_list_values(value_list)))
 
 # The two values are 1124 and 896
 # Multiplying the two is 1007104
