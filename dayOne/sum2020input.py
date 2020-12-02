00237c208cff47ecf5a8e9acba1fcd03c40efb6a
galinabee <galinab@vt.edu>
2020-12-01 21:27:55 -0800

    first star: 1124 and 896 are the values, multiplied to 1007104

--- dayOne/sum2020input.py
+++ dayOne/sum2020input.py
@@ -1,12 +1,24 @@
+import os
+
 def populate_from_file():
-    fp = open('./sum2020input.txt', 'r')
-    input_set = set(fp)
+    print(os.getcwd())
+    fp = open('./dayOne/sum2020input.txt', 'r')
+    input_set = set(int(line.strip()) for line in fp)
     print(input_set)
     fp.close()
     return input_set
 
+def find_two_values(input_set):
+    for val in input_set:
+        if 2020-val in input_set:
+            return val
+
 def main():
-    print("hello")
+    print(os.getcwd())
+    input_set = populate_from_file()
+    val = find_two_values(input_set)
+    print("The two values are {0} and {1}".format(val, 2020-val))
+    print("Multiplying the two is {0}".format((2020-val)*val))
 
 if __name__ == "__main__":
     main()
