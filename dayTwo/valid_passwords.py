9969626f58ff2bfb89a27aafda3c2cb3bacd9677
galinabee <galinab@vt.edu>
2020-12-02 22:29:30 -0800

    cleanup day 2

--- dayTwo/valid_passwords.py
+++ dayTwo/valid_passwords.py
@@ -1,49 +1,35 @@
-def populate_from_file():
-    fp = open('./input.txt', 'r')
+def star(fp, isFirst):
     totalcount = 0
     for line in fp:
         info = line.strip().split(" ")
-        
         numbers = info[0].split("-")
         min = int(numbers[0])
         max = int(numbers[1])
         letter = info[1].split(":")[0]
         string = info[2]
         count = 0
-        for i in string:
-            if i == letter:
+        if isFirst == True:
+            for i in string:
+                if i == letter:
+                    count = count + 1
+            if min <= count and count <= max:
+                totalcount = totalcount + 1
+        else:
+            if string[min-1] == letter:
                 count = count + 1
-        if min <= count and count <= max:
-            totalcount = totalcount + 1
-    fp.close()
+            if string[max-1] == letter:
+                count = count + 1
+            if count == 1:
+                totalcount = totalcount + 1
     print("Total count is {0}".format(totalcount))
-    return totalcount
 
-    #506
-
-def populate_from_file2():
-    fp = open('./input.txt', 'r')
-    totalcount = 0
-    for line in fp:
-        info = line.strip().split(" ")
-        
-        numbers = info[0].split("-")
-        min = int(numbers[0])
-        max = int(numbers[1])
-        letter = info[1].split(":")[0]
-        string = info[2]
-        count = 0
-        if string[min-1] == letter:
-            count = count + 1
-        if string[max-1] == letter:
-            count = count + 1
-        if count == 1:
-            totalcount = totalcount + 1
+def open_file(file):
+    fp = open(file, 'r')
+    star(fp, True)
     fp.close()
-    print("Total count is {0}".format(totalcount))
-    return totalcount
-    #443
+    fp = open(file, 'r')
+    star(fp, False)
+    fp.close()
 
 if __name__ == "__main__":
-    populate_from_file()
-    populate_from_file2()
+    open_file("./input.txt")
