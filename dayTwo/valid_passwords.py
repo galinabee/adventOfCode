15f47d839627355b7653061d9883c9cec2fdb48f
galinabee <galinab@vt.edu>
2020-12-02 21:55:36 -0800

    second star done

--- dayTwo/valid_passwords.py
+++ dayTwo/valid_passwords.py
@@ -21,5 +21,29 @@ def populate_from_file():
 
     #506
 
+def populate_from_file2():
+    fp = open('./input.txt', 'r')
+    totalcount = 0
+    for line in fp:
+        info = line.strip().split(" ")
+        
+        numbers = info[0].split("-")
+        min = int(numbers[0])
+        max = int(numbers[1])
+        letter = info[1].split(":")[0]
+        string = info[2]
+        count = 0
+        if string[min-1] == letter:
+            count = count + 1
+        if string[max-1] == letter:
+            count = count + 1
+        if count == 1:
+            totalcount = totalcount + 1
+    fp.close()
+    print("Total count is {0}".format(totalcount))
+    return totalcount
+    #443
+
 if __name__ == "__main__":
     populate_from_file()
+    populate_from_file2()
